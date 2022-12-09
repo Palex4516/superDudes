64397a76115e3eb80d00174f41ef4b61a0cdc602
Alexander Norgren <alexandernorgren2@gmail.com>
2022-12-09 15:58:29 +0100

    INIT COMMIT 2 BABY

---
+++ src/main/java/se/alex/super_heroes/model/Status.java
@@ -0,0 +1,9 @@
+package se.alex.super_heroes.model;
+
+public enum Status {
+    STAND_BY,
+    ON_MISSION,
+    INJURED,
+    MIA,
+    DEAD
+}
---
+++ src/test/java/se/alex/super_heroes/SuperHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.alex.super_heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
